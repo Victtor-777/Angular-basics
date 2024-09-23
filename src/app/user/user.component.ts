4710e9f8df742ce771727ff4837adc88f4d14463
Victtor777 <victtor.eng.software@gmail.com>
2024-09-23 15:04:13 -0300

    Aprendendo conceitos de input e output e add component tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+} from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // select = output<string>();
 
+  // imagePath = computed(() => {
+  //   return `assets/users/${this.avatar()}`;
+  // });
+
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
