f051b04ca447a1e7aacf5b5ecb86e14e3cf421db
Victtor777 <victtor.eng.software@gmail.com>
2024-09-25 14:18:22 -0300

    Migrando a aplicação para modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { DUMMY_USERS } from './user/dummy-users';
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
